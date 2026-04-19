7e50099c11b8c0fdad16b7f0bc26f0e1db1611f1
kyrstenjunior <kyrstenjr@gmail.com>
2026-04-19 16:52:57 -0300

    feat: criação do componente sidebar e a página dashboard

---
+++ app/components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+  return (
+    <aside className="p-6">
+        <div>
+            <h1 className='font-manrope'>Atelier Workspace</h1>
+            <small>Creative Studio</small>
+        </div>
+    </aside>
+  )
+}
+
+export default Sidebar
